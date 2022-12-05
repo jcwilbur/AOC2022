ee916e9f98f960cdba47dedab90a3d89ab42977f
Joseph Wilbur <joseph@pendo.io>
2022-12-05 15:31:14 -0500

    starting puzzle 2

--- puzzle2/2a.go
+++ puzzle2/2a.go
@@ -5,7 +5,25 @@ import (
 	"fmt"
 )
 
+type round struct {
+	opponentMove string
+	myMove       string
+}
+
 func main() {
 	var inputStrings = helpers.ReadFileAsStrings("input2.txt")
 	fmt.Println(inputStrings)
+	rounds := []round{}
+	for i, line := range inputStrings {
+		rounds = append(rounds, round{opponentMove: line[0:1], myMove: line[2:3]})
+		fmt.Println(rounds[i])
+	}
+
+	for _, v := range rounds {
+		calculatePoints(v)
+	}
+}
+
+func calculatePoints(v round) {
+	panic("unimplemented")
 }
