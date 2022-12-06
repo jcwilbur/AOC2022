df9e4ef6a007ac7ff9ec251b5fecc2018d9a8193
Joseph Wilbur <joseph@pendo.io>
2022-12-06 13:39:08 -0500

    documenting

--- puzzle6/6a.go
+++ puzzle6/6a.go
@@ -9,11 +9,11 @@ const markerLength = 14
 
 func main() {
 	var inputStrings = helpers.ReadFileAsStrings("input6.txt")
-	input := inputStrings[0]
+	input := inputStrings[0] //dont need an array this time, just a string
 
-	for i := 0; i < len(input)-markerLength+1; i++ {
-		candidate := input[i : i+markerLength]
-		if isMarker(candidate) {
+	for i := 0; i < len(input)-markerLength+1; i++ { //loop through each char in the string, stopping in time to not out-of-bounds
+		candidate := input[i : i+markerLength] //grab the candidate marker starting with the active character of length markerLength
+		if isMarker(candidate) {               //if it's a marker, print it and stop looping
 			fmt.Println(candidate)
 			fmt.Println(i + markerLength)
 			break
